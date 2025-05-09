6fb9f6886b9eff3dafa673d389d130a63567e87f
mtgates6 <56485852+mtgates6@users.noreply.github.com>
2025-05-09 16:25:02 -0700

    combine buttons into 1 column

--- src/components/workout/SetRow.tsx
+++ src/components/workout/SetRow.tsx
@@ -22,7 +22,7 @@ export const SetRow: React.FC<SetRowProps> = ({
   handleRemoveSet
 }) => {
   return (
-    <div key={set.id} className="grid grid-cols-4 gap-2 items-center justify-between">
+    <div key={set.id} className="grid grid-cols-3 gap-2 items-center justify-between">
       <Input
         type="number"
         placeholder="Weight"
@@ -37,25 +37,27 @@ export const SetRow: React.FC<SetRowProps> = ({
         onChange={(e) => handleSetUpdate(exerciseIndex, setIndex, 'reps', Number(e.target.value))}
         className='p-2 w-full'
       />
-      <Button
-        variant="outline"
-        onClick={() => handleSetCompletion(setIndex, exerciseIndex, !set.completed)}
-        className="p-0 w-10 h-10 "
-      >
-        {set.completed ? (
-          <CheckCircle2 className="h-3 w-3 text-green-500" />
-        ) : (
-          <Clock className="h-3 w-3" />
-        )}
-      </Button>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => handleRemoveSet(exerciseIndex, setIndex)}
-        className="p-0"
-      >
-        <Trash2 className="h-4 w-4" />
-      </Button>
+      <div className="flex items-center gap-2">
+        <Button
+          variant="outline"
+          onClick={() => handleSetCompletion(setIndex, exerciseIndex, !set.completed)}
+          className="p-0 w-10 h-10"
+        >
+          {set.completed ? (
+            <CheckCircle2 className="h-3 w-3 text-green-500" />
+          ) : (
+            <Clock className="h-3 w-3" />
+          )}
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => handleRemoveSet(exerciseIndex, setIndex)}
+          className="p-0"
+        >
+          <Trash2 className="h-4 w-4" />
+        </Button>
+      </div>
     </div>
   );
 };
