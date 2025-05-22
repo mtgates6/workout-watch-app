9ef7c30ab4d432aae34f66a120c19a5682bb3f5d
mtgates6 <56485852+mtgates6@users.noreply.github.com>
2025-05-22 09:42:58 -0700

    make trash red

--- src/components/workout/ExerciseCard.tsx
+++ src/components/workout/ExerciseCard.tsx
@@ -73,7 +73,7 @@ export const ExerciseCard: React.FC<ExerciseCardProps> = ({
                 size="icon"
                 onClick={() => handleRemoveExercise(exerciseIndex)}
               >
-                <Trash2 className="h-4 w-4" />
+                <Trash2 className="h-4 w-4 text-red-500" />
               </Button>
             </div>
           </div>
